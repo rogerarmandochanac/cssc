6ac0698db3a5ea07378a2e3991d6cd5cf325af61
rogerarmandochanac <armandoroger007@gmail.com>
2025-08-18 01:03:50 -0600

    navbar mobile without efects and events

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hola Mundo</h1>
+    @extends('base')
+    @include('snippets.navbar')
 </body>
 </html>
---
+++ resources/views/snippets/navbar.blade.php
@@ -0,0 +1,4 @@
+<div class="bg-blue" id="navbar">
+    <img src="{{ asset('image/Logo.png') }}" alt="logotipo" width="186px" height="32.42px">
+    <img src="{{ asset('image/menu/amburger.png') }}" alt="amburger" width="26.67px" height="18px">
+</div>
