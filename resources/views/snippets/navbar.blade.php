d018a87f2a64b91c2306765b6d08e43e9fe4ce9c
solucionesnasm <solucionesnasm@gmail.com>
2025-08-25 14:47:24 -0600

    menu mobile section

--- resources/views/snippets/navbar.blade.php
+++ resources/views/snippets/navbar.blade.php
@@ -1,4 +1,15 @@
-<div class="bg-blue" id="navbar">
-    <img src="{{ asset('image/Logo.png') }}" alt="logotipo" width="186px" height="32.42px">
-    <img src="{{ asset('image/menu/amburger.png') }}" alt="amburger" width="26.67px" height="18px">
-</div>
+<nav class="bg-blue">
+    <div id="navbar">
+        <img src="{{ asset('image/Logo.png') }}" alt="logotipo" width="186px" height="32.42px">
+        <ul class="nav_links bg-blue">
+            <li class="nav_list"><a href="#" class="nav_link mona-sans">Inicio</a></li>
+            <li class="nav_list"><a href="#" class="nav_link mona-sans">Nosotros</a></li>
+            <li class="nav_list"><a href="#" class="nav_link mona-sans">Servicios</a></li>
+            <li class="nav_list"><a href="#" class="nav_link mona-sans">Contacto</a></li>
+            <li class="nav_list list_telefono"><a href="" class="nav_link mona-sans">+56 9 5847 36521</a></li>
+            <li class="nav_list list_correo"><a href="" class="nav_link mona-sans">contacto@ccs.cl</a></li>
+        </ul>
+        <a href="#" class="nav_active"><img src="{{ asset('image/menu/close.svg') }}" alt="close icono" class="nav_img" width="50px"></a>
+        <a href="#navbar" class="nav_hamburger"><img src="{{ asset('image/menu/hamburger.svg') }}" alt="close icono" class="nav_img" width="50px"></a>
+    </div>
+</nav>
